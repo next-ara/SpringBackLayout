03b5f50766a7d59a095f16e8ab372af97f57e21b
Afton <next-era@foxmail.com>
2024-05-21 14:54:46 +0800

    回弹布局项目创建

---
+++ app/src/main/java/com/next/view/springback/NestedCurrentFling.java
@@ -0,0 +1,13 @@
+package com.next.view.springback;
+
+/**
+ * ClassName:NestedCurrentFling
+ *
+ * @author Afton
+ * @time 2024/5/21
+ * @auditor
+ */
+public interface NestedCurrentFling {
+
+    boolean onNestedCurrentFling(float f, float f2);
+}
